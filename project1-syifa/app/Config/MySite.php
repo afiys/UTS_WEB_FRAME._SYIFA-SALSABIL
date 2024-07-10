04615e76e16381e2b5ff6b4b2032a06bf293c6c3
afiys <149856784+afiys@users.noreply.github.com>
2024-07-10 14:19:47 +0700

    Add files via upload

---
+++ project1-syifa/app/Config/MySite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Config;
+
+use CodeIgniter\Config\BaseConfig;
+
+class MySite extends BaseConfig
+{
+    public $siteName = "Syifa";
+    public $siteEmail = "syifasalsabil0731@gmail.com";
+}
